6945147addbae6df8eab6d05084a09290a5ce225
Simon Vieille <simon.vieille@free.fr>
2012-04-08 23:27:20 +0200

    Download error status

--- lib/task/InstallationTask.class.php
+++ lib/task/InstallationTask.class.php
@@ -9,31 +9,34 @@ class InstallationTask extends BasicTask {
 		$this->cms_version       = mySfYaml::get('ressources_cms_'.$this->arg_cms.'_versions_'.$this->arg_version);
 		$this->cms_version_url   = mySfYaml::get('ressources_cms_'.$this->arg_cms.'_versions_'.$this->arg_version.'_url');
 
-		if(!$this->cms_configuration) {
-			Cli::printError('Error', $arg_cms.' is not a valid cms. Show cms list by using --list argument.');
+		try {
+			if(!$this->cms_configuration) {
+				throw new CmsInstallerException($arg_cms.' is not a valid cms. Show cms list by using --list argument.');
+			}
+
+			if(!$this->cms_version) {
+				throw new CmsInstallerException($this->arg_version.' is not a valid cms version for '.$this->arg_cms.'. Show cms list by using --list argument.');
+			}
+
+			if(!$this->cms_version_url) {
+				throw new CmsInstallerException($this->arg_cms.' (version '.$this->arg_version.') has not valid configuration (url not found). Show cms list by using --list argument.');
+			}
+
+			Cli::printNotice('CMS: ', $this->arg_cms);
+			Cli::printNotice('Version: ', $this->arg_version);
+			Cli::printNotice('Download from: ', $this->cms_version_url);
+			Cli::printBlankLine();
+
+			$this->tempName = $this->getTempName();
+			$this->download();
+			$this->unpack();			
+		}
+		catch(CmsInstallerException $e) {
+			Cli::printError('Error', $e->getMessage());
 			exit(1);
 		}
 
-		if(!$this->cms_version) {
-			Cli::printError('Error', $this->arg_version.' is not a valid cms version for '.$this->arg_cms.'. Show cms list by using --list argument.');
-			exit(1);
-		}
-
-		if(!$this->cms_version_url) {
-			Cli::printError('Error', $this->arg_cms.' (version '.$this->arg_version.') has not valid configuration (url not found). Show cms list by using --list argument.');
-			exit(1);
-		}	
-
-		Cli::printNotice('CMS: ', $this->arg_cms);
-		Cli::printNotice('Version: ', $this->arg_version);
-		Cli::printNotice('Download from: ', $this->cms_version_url);
-		Cli::printBlankLine();
-
-		$this->tempName = $this->getTempName();
-		$this->download();
-		$this->unpack();
-
-		exit(1);	
+		exit(0);	
 	}
 
 	public function download() {
@@ -45,6 +48,11 @@ class InstallationTask extends BasicTask {
 		$data = curl_exec($curl);
 		curl_close($curl);
 		fclose($fileopen);
+
+		if(!$data) {
+			unlink($this->tempName);	
+			throw new CmsInstallerException('Download has failed.');
+		}
 
 		Cli::printInfo('Finished', '');
 	}
